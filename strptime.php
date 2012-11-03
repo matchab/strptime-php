939feeb372a3be08ae32994d8091c9d6f6f44050
Mathieu Chabanon <git@matchab.fr>
2012-11-03 15:20:39 +0100

    Code style.

--- strptime.php
+++ strptime.php
@@ -10,218 +10,216 @@
 
 if (!function_exists('strptime'))
 {
-
-/**
- * 
- * 
- * @param string $buffer
- * @param string $pattern
- * @return string
- * @access private
- */
-function _strptime_match(&$buffer,$pattern)
-{
-	if (is_array($pattern)){
-		$pattern = implode('|',$pattern);
-	}
-	$pattern = '/^('.$pattern.')/i';
-	
-	$ret = NULL;
-	$matches;
-	if (preg_match($pattern,$buffer,$matches))
+	/**
+	 * 
+	 * 
+	 * @param string $buffer
+	 * @param string $pattern
+	 * @return string
+	 * @access private
+	 */
+	function _strptime_match(&$buffer,$pattern)
 	{
-		$ret = $matches[0];
+		if (is_array($pattern)) {
+			$pattern = implode('|',$pattern);
+		}
+		$pattern = '/^('.$pattern.')/i';
 		
-		//Remove the match from the buffer
-		$buffer = preg_replace($pattern,'',$buffer);
-	}
-	return $ret;
-}
-
-/**
- * 
- * 
- * @param int $n
- * @param int $min
- * @param int $max
- * @return int
- * @access private
- */
-function _strptime_clamp($n,$min,$max){
-	return max(min($n,$max),$min);
-}
-
-/**
- * 
- * 
- * @param string $p
- * @return array
- * @access private
- */
-function _strptime_wdays($p)
-{
-	$locales = array();
-	
-	for ($i=0; $i<7; $i++)
-	{
-		$locales[$i] = strftime('%'.$p,strtotime('next Sunday +'.$i.' days'));
-	}
-	
-	return $locales;
-}
-
-/**
- * 
- * 
- * @param string $p
- * @return array
- * @access private
- */
-function _strptime_months($p)
-{
-	$locales = array();
-	
-	for ($i=1; $i<=12; $i++)
-	{
-		$locales[$i] = strftime('%'.$p,mktime(0,0,0,$i));
-	}
-	
-	return $locales;
-}
-
-/**
- * 
- * 
- * @param string $date
- * @param string $format
- * @return array
- */
-function strptime($date,$format)
-{
-	//Default return values
-	$tm_sec = 0;
-	$tm_min = 0;
-	$tm_hour = 0;
-	$tm_mday = 1;
-	$tm_mon = 1;
-	$tm_year = 1900;
-	$tm_wday = 0;
-	$tm_yday = 0;
-	
-	$buffer = $date;
-	$length = strlen($format);
-	$lastc = NULL;
-	
-	for ($i=0; $i<$length; $i++)
-	{
-		$c = $format[$i];
-		
-		//Remove spaces
-		$buffer = ltrim($buffer);
-		
-		if ($lastc == '%')
+		$ret = null;
+		$matches;
+		if (preg_match($pattern,$buffer,$matches))
 		{
-			switch ($c)
-			{
-				case 'A':
-				case 'a':
-					_strptime_match($buffer,_strptime_wdays($c));
-					break;
-					
-				case 'B':
-				case 'b':
-				case 'h':
-					$months = _strptime_months($c);
-					$month = _strptime_match($buffer,$months);
-					$tm_mon = array_search($month,$months);
-					break;
-					
-				case 'D':
-					//Unsupported by strftime on Windows
-					_strptime_match($buffer,'\d{2}\/\d{2}\/\d{2}');
-					break;
-					
-				//case 'e':
-				case 'd':
-					$tm_mday = intval(_strptime_match($buffer,'\d{2}'));
-					break;
-					
-				case 'F':
-					//Unsupported by strftime on Windows
-					if ($ret = _strptime_match($buffer,'\d{4}-\d{2}-\d{2}'))
-					{
-						$frags = explode('-',$ret);
-						$tm_year = intval($frags[0]);
-						$tm_mon = intval($frags[1]);
-						$tm_mday = intval($frags[2]);
-					}
-					break;
-					
-				case 'H':
-					$tm_hour = intval(_strptime_match($buffer,'\d{2}'));
-					break;
-					
-				case 'M':
-					$tm_min = intval(_strptime_match($buffer,'\d{2}'));
-					break;
-					
-				case 'm':
-					$tm_mon = intval(_strptime_match($buffer,'\d{2}'));
-					break;
-					
-				case 'S':
-					$tm_sec = intval(_strptime_match($buffer,'\d{2}'));
-					break;
-					
-				case 'Y':
-					$tm_year = intval(_strptime_match($buffer,'\d{4}'));
-					break;
-					
-				case 'y':
-					$year = intval(_strptime_match($buffer,'\d{2}'));
-					if ($year < 69) {
-						$tm_year = 2000 + $year;
-					} else {
-						$tm_year = 1900 + $year;
-					}
-					break;
-					
-			}
+			$ret = $matches[0];
+			
+			//Remove the match from the buffer
+			$buffer = preg_replace($pattern,'',$buffer);
 		}
-		else{
-			$buffer = ltrim($buffer,$c);
+		return $ret;
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @param int $n
+	 * @param int $min
+	 * @param int $max
+	 * @return int
+	 * @access private
+	 */
+	function _strptime_clamp($n,$min,$max) {
+		return max(min($n,$max),$min);
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @param string $p
+	 * @return array
+	 * @access private
+	 */
+	function _strptime_wdays($p)
+	{
+		$locales = array();
+		
+		for ($i = 0; $i < 7; $i++)
+		{
+			$dayTime = strtotime('next Sunday +'.$i.' days');
+			$locales[$i] = strftime('%'.$p,$dayTime);
 		}
 		
-		$lastc = $c;
+		return $locales;
 	}
 	
-	//Date must exists!
-	if (!checkdate($tm_mon,$tm_mday,$tm_year)){
-		return false;
+	/**
+	 * 
+	 * 
+	 * @param string $p
+	 * @return array
+	 * @access private
+	 */
+	function _strptime_months($p)
+	{
+		$locales = array();
+		
+		for ($i = 1; $i <= 12; $i++) {
+			$locales[$i] = strftime('%'.$p,mktime(0,0,0,$i));
+		}
+		
+		return $locales;
 	}
 	
-	//Clamp hours values
-	$tm_sec = _strptime_clamp($tm_sec,0,61); //Leap seconds
-	$tm_min = _strptime_clamp($tm_min,0,59);
-	$tm_hour = _strptime_clamp($tm_hour,0,23);
-	
-	//Compute wday and yday
-	$timestamp = mktime($tm_hour,$tm_min,$tm_sec,$tm_mon,$tm_mday,$tm_year);
-	$tm_wday = date('w',$timestamp);
-	$tm_yday = date('z',$timestamp);
-	
-	//Return
-	$time = array();
-	$time['tm_sec'] = $tm_sec;
-	$time['tm_min'] = $tm_min;
-	$time['tm_hour'] = $tm_hour;
-	$time['tm_mday'] = $tm_mday;
-	$time['tm_mon'] = ($tm_mon-1); //0-11
-	$time['tm_year'] = ($tm_year-1900);
-	$time['tm_wday'] = $tm_wday;
-	$time['tm_yday'] = $tm_yday;
-	$time['unparsed'] = $buffer; //Unparsed buffer
-	return $time;
+	/**
+	 * 
+	 * 
+	 * @param string $date
+	 * @param string $format
+	 * @return array
+	 */
+	function strptime($date,$format)
+	{
+		//Default return values
+		$tmSec  = 0;
+		$tmMin  = 0;
+		$tmHour = 0;
+		$tmMday = 1;
+		$tmMon  = 1;
+		$tmYear = 1900;
+		$tmWday = 0;
+		$tmYday = 0;
+		
+		$buffer = $date;
+		$length = strlen($format);
+		$lastc = null;
+		
+		for ($i = 0; $i < $length; $i++)
+		{
+			$c = $format[$i];
+			
+			//Remove spaces
+			$buffer = ltrim($buffer);
+			
+			if ($lastc == '%')
+			{
+				switch ($c)
+				{
+					case 'A':
+					case 'a':
+						_strptime_match($buffer,_strptime_wdays($c));
+						break;
+						
+					case 'B':
+					case 'b':
+					case 'h':
+						$months = _strptime_months($c);
+						$month = _strptime_match($buffer,$months);
+						$tmMon = array_search($month,$months);
+						break;
+						
+					case 'D':
+						//Unsupported by strftime on Windows
+						_strptime_match($buffer,'\d{2}\/\d{2}\/\d{2}');
+						break;
+						
+					//case 'e':
+					case 'd':
+						$tmMday = intval(_strptime_match($buffer,'\d{2}'));
+						break;
+						
+					case 'F':
+						//Unsupported by strftime on Windows
+						if ($ret = _strptime_match($buffer,'\d{4}-\d{2}-\d{2}'))
+						{
+							$frags  = explode('-',$ret);
+							$tmYear = intval($frags[0]);
+							$tmMon  = intval($frags[1]);
+							$tmMday = intval($frags[2]);
+						}
+						break;
+						
+					case 'H':
+						$tmHour = intval(_strptime_match($buffer,'\d{2}'));
+						break;
+						
+					case 'M':
+						$tmMin = intval(_strptime_match($buffer,'\d{2}'));
+						break;
+						
+					case 'm':
+						$tmMon = intval(_strptime_match($buffer,'\d{2}'));
+						break;
+						
+					case 'S':
+						$tmSec = intval(_strptime_match($buffer,'\d{2}'));
+						break;
+						
+					case 'Y':
+						$tmYear = intval(_strptime_match($buffer,'\d{4}'));
+						break;
+						
+					case 'y':
+						$year = intval(_strptime_match($buffer,'\d{2}'));
+						if ($year < 69) {
+							$tmYear = 2000 + $year;
+						} else {
+							$tmYear = 1900 + $year;
+						}
+						break;
+						
+				}
+			}
+			else {
+				$buffer = ltrim($buffer,$c);
+			}
+			
+			$lastc = $c;
+		}
+		
+		//Date must exists!
+		if (!checkdate($tmMon,$tmMday,$tmYear)) {
+			return false;
+		}
+		
+		//Clamp hours values
+		$tmHour = _strptime_clamp($tmHour,0,23);
+		$tmMin = _strptime_clamp($tmMin,0,59);
+		$tmSec = _strptime_clamp($tmSec,0,61); //>59 = Leap seconds
+		
+		//Compute wday and yday
+		$timestamp = mktime($tmHour,$tmMin,$tmSec,$tmMon,$tmMday,$tmYear);
+		$tmWday = date('w',$timestamp);
+		$tmYday = date('z',$timestamp);
+		
+		//Return
+		$time = array();
+		$time['tm_sec']   = $tmSec;
+		$time['tm_min']   = $tmMin;
+		$time['tm_hour']  = $tmHour;
+		$time['tm_mday']  = $tmMday;
+		$time['tm_mon']   = ($tmMon-1); //0-11
+		$time['tm_year']  = ($tmYear-1900);
+		$time['tm_wday']  = $tmWday;
+		$time['tm_yday']  = $tmYday;
+		$time['unparsed'] = $buffer; //Unparsed buffer
+		return $time;
+	}
 }
-}
-?>
